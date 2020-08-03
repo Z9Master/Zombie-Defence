feaf8e4eaa849ca638ff3e4f6e893ad61b1c64d1
Z9Master <34867938+Z9Master@users.noreply.github.com>
2020-08-03 20:34:12 +0200

    uPDATE
    1

---
+++ Zombie Defence/Assets/Scripts/DayNIght/LightningPreset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lightning Preset", menuName = "Scriptable Objects/Lightning Preset", order = 1)]
+public class LightningPreset : ScriptableObject
+{
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+}
