09773a0bb5ae1d080b5ed3bff73356474d7145b1
Z9Master <34867938+Z9Master@users.noreply.github.com>
2020-08-04 21:15:14 +0200

    Update 1.1
    Hráč
    Hráč se teď může pohybovat pomocí WASD
    Pokud v detekovacím zóně nejsou nepřátelé, dívá se směrem pohybu.
    Pokud je víc nepřítelů v detekovacím zóně hráče, hráč se dívá na nejbližší nepřítel.
    (důvod: až hráč bude mít zbraň, bude móci střílet na nepřítel bez míření, pokud je nepřítel v detekovacím zóně, pokud není, hráč střílí směrem, kterým je otočený.)
    (proč nemíříme myším? -> moc komplikovaný)
    
    Enemy
    Zatím nemá detekovací zónu.
    Míří buď na hráče nebo na turet, záleží na tom, co je bližší.
    
    Level
    Zatím demo, budou tam jiné objekty

---
+++ Zombie Defence/Assets/Scripts/Character Controler/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // COmment zitra
+    public CharacterController playerController;
+
+    public float speed = 5f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public float detectorDistance = 10f;
+
+    public GameObject[] targetComplex;
+
+    private GameObject target;
+
+    void Start()
+    {
+        targetComplex = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+    void Update()
+    {
+        selectNearTarget();
+        PlayerMove();                               
+    }
+
+    private void selectNearTarget()
+    {
+        float lenght;
+        float nearest = Mathf.Infinity;
+        foreach (GameObject tg in targetComplex)
+        {
+            lenght = Vector3.Distance(tg.transform.position, transform.position);
+            if(lenght < nearest)
+            {
+                nearest = lenght;
+                target = tg;
+            }
+        }
+    }
+
+    private void PlayerMove()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if(Vector3.Distance(transform.position, target.transform.position) > detectorDistance)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+            else
+            {
+                Vector3 direction2 = target.transform.position - transform.position;
+                float targetAngle2 = Mathf.Atan2(direction2.x, direction2.z) * Mathf.Rad2Deg;
+                float angle2 = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle2, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle2, 0f);
+            }         
+
+            playerController.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Zombie Defence/Assets/Scripts/Enemy/EnemyControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public GameObject targetPlayer;
+    public GameObject targetTuret;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        if(Vector3.Distance(transform.position, targetPlayer.transform.position) < Vector3.Distance(transform.position, targetTuret.transform.position))
+        {
+            agent.SetDestination(targetPlayer.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(targetTuret.transform.position);
+        }
+    }
+}
