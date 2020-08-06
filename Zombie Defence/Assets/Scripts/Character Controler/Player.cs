bdd836ea7fb08e45fa611ff707ca7cb489d65db4
Z9Master <34867938+Z9Master@users.noreply.github.com>
2020-08-06 18:45:56 +0200

    Update 1.2 - Turet update
    Turet
    Míří na nejbližší nepřítel, pokud je v zóně
    
    Player
    jestli stiskneme space, hráč začne mířit na nejbližší nepřítel, pokud je v zóně, ale přitom se nemůže hýbat.

--- Zombie Defence/Assets/Scripts/Character Controler/Player.cs
+++ Zombie Defence/Assets/Scripts/Character Controler/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
     // COmment zitra
     public CharacterController playerController;
 
-    public float speed = 5f;
+    public float PlayerSpeed = 3f;
+    private float speed;
+    private float SideSpeed;
+
 
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
@@ -18,14 +21,19 @@ public class Player : MonoBehaviour
 
     private GameObject target;
 
+    private bool isLooking = false;
+
     void Start()
     {
+        speed = PlayerSpeed;
+        SideSpeed = speed - 0.8f;
         targetComplex = GameObject.FindGameObjectsWithTag("Enemy");
     }
     void Update()
     {
         selectNearTarget();
-        PlayerMove();                               
+        PlayerMove();
+        LookAtEnemy();
     }
 
     private void selectNearTarget()
@@ -48,24 +56,74 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        if (!isLooking)
+        {
+            if (horizontal == 1 && vertical == 1)
+            {
+                speed = SideSpeed;
+            }
+            else if (horizontal == -1 && vertical == -1)
+            {
+                speed = SideSpeed;
+            }
+            else if (horizontal == 1 && vertical == -1)
+            {
+                speed = SideSpeed;
+            }
+            else if (horizontal == -1 && vertical == 1)
+            {
+                speed = SideSpeed;
+            }
+            else
+            {
+                speed = PlayerSpeed;
+            }
+        }
+
+        
 
         if (direction.magnitude >= 0.1f)
         {
-            if(Vector3.Distance(transform.position, target.transform.position) > detectorDistance)
+            //if(Vector3.Distance(transform.position, target.transform.position) > detectorDistance)
+            //{
+            //    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //}
+            //else
+            //{
+            //    Vector3 direction2 = target.transform.position - transform.position;
+            //    float targetAngle2 = Mathf.Atan2(direction2.x, direction2.z) * Mathf.Rad2Deg;
+            //    float angle2 = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle2, ref turnSmoothVelocity, turnSmoothTime);
+            //    transform.rotation = Quaternion.Euler(0f, angle2, 0f);
+            //}
+
+            if (!isLooking)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
             }
-            else
-            {
-                Vector3 direction2 = target.transform.position - transform.position;
-                float targetAngle2 = Mathf.Atan2(direction2.x, direction2.z) * Mathf.Rad2Deg;
-                float angle2 = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle2, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle2, 0f);
-            }         
 
             playerController.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
+    private void LookAtEnemy()
+    {
+        if (Input.GetKey("space") && Vector3.Distance(transform.position, target.transform.position) < detectorDistance)
+        {
+            isLooking = true;
+            Vector3 direction2 = target.transform.position - transform.position;
+            float targetAngle2 = Mathf.Atan2(direction2.x, direction2.z) * Mathf.Rad2Deg;
+            float angle2 = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle2, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle2, 0f);
+            speed = 0;
+        }
+        else
+        {
+            isLooking = false;
+            speed = PlayerSpeed;
         }
     }
 }
---
+++ Zombie Defence/Assets/Scripts/Turet/TuretControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuretControl : MonoBehaviour
+{
+    public GameObject[] targetComplex;
+
+    private GameObject target;
+
+    public GameObject turetHead;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public float turetRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetComplex = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectNearTarget();
+        
+    }
+
+    private void selectNearTarget()
+    {
+        float lenght;
+        float nearest = Mathf.Infinity;
+        foreach (GameObject tg in targetComplex)
+        {
+            lenght = Vector3.Distance(tg.transform.position, turetHead.transform.position);
+            if (lenght < nearest)
+            {
+                nearest = lenght;
+                target = tg;
+                TuretRotate();
+            }
+        }
+    }
+    void TuretRotate()
+    {
+        if(Vector3.Distance(turetHead.transform.position, target.transform.position) <= turetRange)
+        {
+            Vector3 direction = target.transform.position - turetHead.transform.position;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(turetHead.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            turetHead.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+    }
+
+}
